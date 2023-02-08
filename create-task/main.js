f3a66a4acccda141fbfe81f3f88cb570eb42b543
joannec34 <joannechen9058@gmail.com>
2023-02-08 14:00:56 -0500

    pokpoke

--- create-task/main.js
+++ create-task/main.js
@@ -1,1 +1,24 @@
 import "./style.css";
+
+const DOMSelectors = {
+  genImage: document.querySelector("#img-btn"),
+  toggleHist: document.querySelector("#hist-btn"),
+  histSection: document.querySelector("#history"),
+};
+
+const histSection = DOMSelectors.histSection;
+const history = [];
+
+let url = `https://pokeapi.co/api/v2/pokemon/{${id}}/`;
+
+async function fetchData() {
+  try {
+    const rawData = await fetch(url);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function display(arr, section) {
+  section.innerHTML = "";
+}
