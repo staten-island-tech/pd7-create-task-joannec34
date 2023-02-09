f4026001562f0f8dbbfc59ad7ba0e8aec8f75251
joannec34 <joannechen9058@gmail.com>
2023-02-09 14:01:04 -0500

    poke

--- create-task/main.js
+++ create-task/main.js
@@ -2,6 +2,7 @@ import "./style.css";
 
 const DOMSelectors = {
   genImage: document.querySelector("#img-btn"),
+  section: document.querySelector("#api-reponse"),
   toggleHist: document.querySelector("#hist-btn"),
   histSection: document.querySelector("#history"),
 };
@@ -9,16 +10,23 @@ const DOMSelectors = {
 const histSection = DOMSelectors.histSection;
 const history = [];
 
-let url = `https://pokeapi.co/api/v2/pokemon/{${id}}/`;
+let id = Math.floor(Math.random() * 1000);
+let url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
 
-async function fetchData() {
+async function fetchData(url, id) {
+  console.log(id);
+  console.log(url);
   try {
-    const rawData = await fetch(url);
+    const response = await fetch(url);
+    const rawData = await response.json();
+    display(rawData, DOMSelectors.section);
+    console.log(rawData);
   } catch (error) {
     console.log(error);
   }
 }
+fetchData(url, id);
 
-function display(arr, section) {
-  section.innerHTML = "";
+function display(arr, div) {
+  div.innerHTML = `<img src="${arr.sprites.front_default}" alt="">`;
 }
