6db5f69fbdd95a85d147837e5ae20a7c5ab589c8
joannec34 <joannechen9058@gmail.com>
2023-02-14 14:01:31 -0500

    cvbn

--- create-task/main.js
+++ create-task/main.js
@@ -2,6 +2,7 @@ import "./style.css";
 
 const DOMSelectors = {
   genImage: document.querySelector("#img-btn"),
+  body: document.querySelector("#body"),
   section: document.querySelector("#api-reponse"),
   toggleHist: document.querySelector("#hist-btn"),
   histSection: document.querySelector("#history"),
@@ -23,32 +24,38 @@ async function fetchData(url, id) {
     const response = await fetch(url);
     const rawData = await response.json();
     console.log(rawData.name);
-    display(rawData, DOMSelectors.section);
+    displayPoke(rawData, DOMSelectors.section);
     afterGuess(rawData);
     console.log(rawData);
+    reset();
   } catch (error) {
     console.log(error);
   }
 }
 fetchData(url, id);
 
-function display(arr, div) {
-  div.innerHTML = `<img src="${arr.sprites.front_default}" alt="">`;
+function displayPoke(arr, div) {
+  div.innerHTML = `
+  <img src="${arr.sprites.front_default}">`;
 }
 
 function afterGuess(arr) {
-  DOMSelectors.form.addEventListener("submit", function (e) {
-    e.preventDefault();
+  DOMSelectors.form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    DOMSelectors.form.remove();
     let input = DOMSelectors.userInput.value;
     //console.log(input);
     if (arr.name.includes(`${input}`)) {
       console.log("right");
-      DOMSelectors.section.innerHTML = `<img src="${arr.sprites.front_default}" alt=""> ur right yay the pokemon is ${arr.name} <button id="reset-btn">new pokemon</button>`;
+      DOMSelectors.section.innerHTML = `
+      <img src="${arr.sprites.front_default}" alt="">
+      <p> ur right yay the pokemon is ${arr.name} </p>`;
     } else {
       console.log("wrong");
-      DOMSelectors.section.innerHTML = `<img src="${arr.sprites.front_default}" alt=""> ur wrong grr the pokemon is ${arr.name} <button id="reset-btn">new pokemon</button>`;
+      DOMSelectors.section.innerHTML = `
+      <img src="${arr.sprites.front_default}" alt="">
+      <p> ur wrong grr the pokemon is ${arr.name} </p>`;
     }
-    reset();
   });
 }
 
@@ -56,7 +63,7 @@ function reset() {
   let resetBtn = document.querySelector("#reset-btn");
   console.log(resetBtn);
   resetBtn.addEventListener("click", function () {
-    DOMSelectors.section.innerHTML = "";
+    DOMSelectors.body.innerHTML = "";
     location.reload();
   });
 }
